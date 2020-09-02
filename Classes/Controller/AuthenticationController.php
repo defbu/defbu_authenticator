2d8c93d3369518364ff9cbf581b55ded2f58d58d
Frank Buijze <frank@buijze.eu>
2020-09-02 17:56:24 +0200

    Test

---
+++ Classes/Controller/AuthenticationController.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+/**
+ * Copyright FraJa WeB - DEFBU (c) 2019
+ */
+namespace FraJaWeB\FwAutenticator\Controller;
+
+/**
+ * Controller Abstract
+ *
+ * @author Frank Buijze - User Sense <frank@usersense.nl>
+ *
+ * All rights reserved
+ */
+class AuthenticationController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController {
+
+
+}
