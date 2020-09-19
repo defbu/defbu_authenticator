7bee2498bcafd530f98fbbbd3f794b3e7de3461a
Frank Buijze <frank@buijze.eu>
2020-09-20 00:24:48 +0200

    Test

--- ext_tables.php
+++ ext_tables.php
@@ -8,6 +8,9 @@
  */
 defined('TYPO3_MODE') || die();
 
+
+$relativePath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('FraJaWeB.FwAuthenticator');
+echo $relativePath;
 $GLOBALS['TBE_STYLES']['stylesheet2'] = 'EXT:fw_authenticator/Resources/Public/Css/fw_authenticator.css';
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
