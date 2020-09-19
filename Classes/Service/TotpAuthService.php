bd257a59354704a7bb4a5bde241fceb437f2aaa7
Frank Buijze <frank@buijze.eu>
2020-09-19 22:37:55 +0200

    Test

--- Classes/Service/TotpAuthService.php
+++ Classes/Service/TotpAuthService.php
@@ -1,29 +1,25 @@
 <?php
-/**
- * Copyright User Sense (c) 2019
- */
 namespace FraJaWeB\FwAuthenticator\Service;
 
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Frontend\Authentication\FrontendUserAuthentication;
-
-/**
- * Authentication Login
- *
- * @author Frank Buijze - FraJa WeB
- *
- * All rights reserved
- */
-class TotpAuthService extends \TYPO3\CMS\Core\Authentication\AuthenticationService
+class TotpAuthService extends \TYPO3\CMS\Core\Authenticatn\AbstractAuthenticationService
 {
+    /**
+     *
+     * @var \FraJaWeB\FwAuthenticator\Service\TotpService
+     */
+    private $totpService = null;
 
-    public function authUser(array $user) {
-        
-        if ($user["fw_authenticator_active"])
-        
-        
-        return 0;
+    public function init() {
+        $available = false;
+        $this->totpService = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstanceService('totp');
+        if (is_object($this->totpService)) {
+            $available = true;
+        }
+        return $available;
     }
 
+    public function authUser($user) {
+        return -1;
+    }
 
 }
