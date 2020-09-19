b8550aee9dceb3120395eb09247557e8c2c744dd
Frank Buijze <frank@buijze.eu>
2020-09-19 19:43:14 +0200

    Test

--- Classes/Service/TotpAuthenticationService.php
+++ Classes/Service/TotpAuthenticationService.php
@@ -2,7 +2,7 @@
 /**
  * Copyright User Sense (c) 2019
  */
-namespace FraJaWeb\FwAuthenticator\Service;
+namespace FraJaWeB\FwAuthenticator\Service;
 
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Frontend\Authentication\FrontendUserAuthentication;
@@ -16,12 +16,6 @@ use TYPO3\CMS\Frontend\Authentication\FrontendUserAuthentication;
  */
 class TotpAuthenticationService extends \TYPO3\CMS\Core\Authentication\AbstractAuthenticationService
 {
-    public function initAuth($mode, $loginData, $authInfo, $pObj) {
-
-
-        parent::initAuth($mode, $loginData, $authInfo, $pObj);
-    }
-
     public function authUser(array $user) {
         print_r($user);
         return -1;
