21b1e33c4a26df508ea8c5a8e62ded48844634ba
Frank Buijze <frank@buijze.eu>
2020-09-19 15:06:40 +0200

    Test

---
+++ Classes/Service/TotpService.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+/**
+ * Copyright FraJa WeB - DEFBU (c) 2019
+ */
+namespace FraJaWeB\FwAuthenticator\Service;
+
+/**
+ * Controller Abstract
+ *
+ * @author Frank Buijze - User Sense <frank@usersense.nl>
+ *
+ * All rights reserved
+ */
+
+use TYPO3\CMS\Extbase\Annotation as Extbase;
+use TYPO3\CMS\Core\Core\Environment as Environment;
+
+class TotpService extends\TYPO3\CMS\Core\Service\AbstractService {
+
+    public function init() {
+
+    }
+
+    public function reset() {
+
+    }
+
+}
--- ext_localconf.php
+++ ext_localconf.php
@@ -9,3 +9,28 @@
 defined('TYPO3_MODE') || die();
 
 //$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][TYPO3\CMS\Frontend\Authentication\FrontendUserAuthentication::class] = ['className' =>  UserSense\UserSense\Authentication\Login\StandardLogin::class];
+
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addService(
+    // Extension Key
+    'fw_authenticator',
+    // Service type
+    'authentication',
+    // Service key
+    'tx_fwauthenticator_totp',
+    array(
+        'title' => 'Authenticator TOTP',
+        'description' => 'Authenticator TOTP',
+
+        'subtype' => '',
+
+        'available' => true,
+        'priority' => 60,
+        'quality' => 80,
+
+        'os' => '',
+        'exec' => '',
+
+        'className' => \FraJaWeB\FwAuthenticator\Service\TotpService::class
+    )
+);
